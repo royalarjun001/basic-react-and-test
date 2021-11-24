5f14f7f42104ea259e60d90f5513e861bde1d7ce
Arjun kumar verma <arjun.kumar.verma@jci.com>
2021-11-24 14:29:28 +0530

    Add Baisc routing to demo app

---
+++ src/components/StateExample.js
@@ -0,0 +1,9 @@
+const StateExample = () => {
+    return (
+        <div>
+            <h2>useState Example</h2>
+        </div>
+     );
+}
+ 
+export default StateExample;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import StoreLocator from './StoreLocator';
+import StateExample from '../components/StateExample';
+import { Link, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div data-testid="app-element" className="app-container">
-      <StoreLocator/>
+        <ul className="navbar-nav mr-auto">
+          <li><Link to={'/'} className="nav-link"> Store Locator </Link></li>
+          <li><Link to={'/use-state'} className="nav-link"> useState </Link></li>
+        </ul>
+      <Routes>
+        <Route path='/' element={<StoreLocator />} />
+        <Route path='/use-state' element={<StateExample />} />
+      </Routes>
     </div>
   );
 }
